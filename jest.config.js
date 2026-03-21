d368d0adda04942d07ad9a1857421709d3da5f2f
Cynthiamay <cynthiayamaoto@gmail.com>
2026-03-21 18:39:47 -0300

    chore: initial configruations

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  }
+};
